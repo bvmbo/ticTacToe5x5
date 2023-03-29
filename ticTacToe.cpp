a923ee635947e11bd3a39c28010ca2706733725b
bvmbo <wiktorsz2001@wp.pl>
2023-03-29 20:00:58 +0200

    final version

--- ticTacToe.cpp
+++ ticTacToe.cpp
@@ -5,97 +5,137 @@ using namespace std;
 
 const int SIZE = 5;
 char board[SIZE][SIZE];
-bool a = 0, b = 0;
 
-bool check_row(int row, char player) {
-    for (int col = 0; col < SIZE; col++) {
-        if (board[row][col] != player) {
-            return false;
-        }
-    }
-    if (player == 'A') 
-        a = 1;
-    else
-        b = 1;
-    return true;
+bool check_vertical(int col, char player) {
+	int count = 0;
+
+	for (int row = 0; row < SIZE; row++) {
+		if (board[row][col] != player) {
+			count = 0;
+		}
+		else {
+			count++;
+		}
+
+		if (count == 3) {
+
+			return true;
+		}
+	}
+	return false;
 }
 
-bool check_col(int col, char player) {
-    for (int row = 0; row < SIZE; row++) {
-        if (board[row][col] != player) {
-            return false;
-        }
-    }
+bool check_horizontal(int row, char player) {
+	int count = 0;
 
-    if (player == 'A')
-        a = 1;
-    else
-        b = 1;
-    return true;
+	for (int col = 0; col < SIZE; col++) {
+		if (board[row][col] != player) {
+			count = 0;
+		}
+		else {
+			count++;
+		}
+
+		if (count == 3) {
+
+			return true;
+		}
+	}
+	return false;
 }
 
 bool check_diag(char player) {
-    int count1 = 0;
-    int count2 = 0;
-    for (int i = 0; i < SIZE; i++) {
-        if (board[i][i] == player) {
-            count1++;
-        }
-        if (board[i][SIZE - 1 - i] == player) {
-            count2++;
-        }
-    }
+	int row = 0;
+	int col = 0;
+	int count = 0;
 
-    if (player == 'A')
-        a = 1;
-    else
-        b = 1;
+	for (int c = col - 2; c <= col + 2; c++) {
+		if (c < SIZE && c >= 0) {
+			if (board[row][c] != player) {
+				count = 0;
+			}
+			else {
+				count++;
+			}
 
-    return (count1 == 3 || count2 == 3);
+			if (count == 3) {
+
+				return true;
+			}
+		}
+		row++;
+	}
+
+	count = 0;
+	row = 0;
+
+	for (int c = 6; c >= 2; c--) {
+		if (c < SIZE && c >= 0) {
+			if (board[row][c] != player) {
+				count = 0;
+			}
+			else {
+				count++;
+			}
+
+			if (count == 3) {
+
+				return true;
+			}
+		}
+		row++;
+	}
+
+
+	return false;
 }
 
 bool check_win(char player) {
-    for (int i = 0; i < SIZE; i++) {
-        if (check_row(i, player) || check_col(i, player)) {
-            return true;
-        }
-    }
-    return check_diag(player);
+	for (int i = 0; i < SIZE; i++) {
+		if (check_horizontal(i, player) || check_vertical(i, player)) {
+			return true;
+		}
+	}
+	return false;
 }
 
 bool check_draw() {
-    if (a && b)
-        return true;
-    else
-        return false;
+	bool a_wins = check_win('A');
+	bool b_wins = check_win('B');
+
+
+	if (!a_wins) a_wins = check_diag('A');
+	if (!b_wins) b_wins = check_diag('B');
+
+	return ((!a_wins && !b_wins) || (a_wins && b_wins));
 }
 
 void play_game() {
-    string line;
-    for (int row = 0; row < SIZE; row++) {
-        cin >> line;
-        for (int col = 0; col < SIZE; col++) {
-            board[row][col] = line[col];
-        }
-    }
+	string line;
+	for (int i = 0; i < SIZE; i++) {
+		cin >> line;
+		for (int j = 0; j < SIZE; j++) {
+			board[i][j] = line[j];
+		}
+	}
 
-    if (check_win('A')) {
-        cout << "A wygrywa" << endl;
-    }
-    else if (check_win('B')) {
-        cout << "B wygrywa" << endl;
-    }
-    else if (check_draw()) {
-        cout << "remis" << endl;
-    }
+	if (check_draw()) {
+		cout << "remis" << endl;
+	}
+	else if (check_win('A')) {
+		cout << "A wygrywa" << endl;
+	}
+	else if (check_win('B')) {
+		cout << "B wygrywa" << endl;
+	}
 }
 
-int main(){
-	int cases;
-	cin >> cases;
-	while (cases--) {
-        play_game();
-	}
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--)
+		play_game();
 
 	return 0;
 }
